9e848b12cdcf8bdbd32a52226f390ee1a235ac7e
Kasen <18170166+imkasen@users.noreply.github.com>
2022-06-09 17:19:44 +0800

    use lockfile to make sure only one program can run

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtCore import QLockFile, QDir
 from gui.main_window import MainWindow
 
 
@@ -7,12 +8,22 @@ def main() -> None:
     the entry function of the application
     :return: None
     """
-    app = QApplication([])  # only one QApplication instance per application, no command line arguments
-
-    window = MainWindow()
-    window.show()  # windows are hidden by default
-
-    app.exec()  # start the event loop
+    lock_file = QLockFile(QDir.tempPath() + "/unsplash_wallpaper.lock")  # make sure only one program can run
+    try:
+        app = QApplication([])  # only one QApplication instance per application, no command line arguments
+        if lock_file.tryLock():
+            window = MainWindow()
+            window.show()  # windows are hidden by default
+            app.exec()  # start the event loop
+        else:
+            err_msg = QMessageBox()
+            err_msg.setIcon(QMessageBox.Warning)
+            err_msg.setWindowTitle("Error")
+            err_msg.setText("The application is already running!")
+            err_msg.setStandardButtons(QMessageBox.Ok)
+            err_msg.exec()
+    finally:
+        lock_file.unlock()
 
 
 if __name__ == "__main__":

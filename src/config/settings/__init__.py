f48ca2a0b38f8b961123b680c3cefd538bfbe05e
Kasen <18170166+imkasen@users.noreply.github.com>
2022-07-26 22:33:25 +0800

    add a function to get args from settings.json

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -1,1 +1,2 @@
 from .create_settings import create_settings
+from .get_settings_arg import get_settings_arg
---
+++ src/config/settings/get_settings_arg.py
@@ -0,0 +1,29 @@
+from PySide6.QtCore import QFile, QIODevice, QTextStream
+from ..args import PATH
+import logging
+import json
+
+
+def get_settings_arg(arg_name: str) -> (bool, str):
+    """
+    Get the configuration in the settings based on the input string.
+    :param arg_name: configuration key
+    :return tuple: (bool, configuration value)
+    """
+    logger: logging.Logger = logging.getLogger(__name__)
+    settings_file: QFile = QFile(PATH["CONFIG"] + "settings.json")
+    res: tuple = (False, "")
+    if settings_file.exists():
+        if settings_file.open(QIODevice.ReadOnly | QIODevice.Text | QIODevice.ExistingOnly):
+            stream: QTextStream = QTextStream(settings_file)
+            val: str = json.loads(stream.readAll()).get(arg_name, "")
+            if val:
+                res = (True, val)
+            else:
+                logger.error("Fail to get the argument because the key is not existed")
+        else:
+            logger.error("Fail to open 'settings.json' when trying to read an argument")
+    else:
+        logger.error("'settings.json' is not existed when trying to read an argument")
+    settings_file.close()
+    return res

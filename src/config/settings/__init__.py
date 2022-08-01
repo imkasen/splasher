78bd38a65647a599fec858be1d63b685d46b8e38
Kasen <18170166+imkasen@users.noreply.github.com>
2022-08-01 21:46:18 +0800

    add a function to set args into settings.json

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -1,2 +1,3 @@
 from .create_settings import create_settings
 from .get_settings_arg import get_settings_arg
+from .set_settings_arg import set_settings_arg
---
+++ src/config/settings/set_settings_arg.py
@@ -0,0 +1,65 @@
+from PySide6.QtCore import QFile, QIODevice, QTextStream
+from ..args import PATH
+from typing import Optional, Any
+import logging
+import json
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def set_settings_arg(arg_key: str, arg_value: str) -> bool:
+    """
+    Modify the configuration value in the settings based on the input key.
+    :param arg_key: the configuration key that needs to be updated
+    :param arg_value: the configuration value for update
+    :return: bool
+    """
+    res: bool = False
+    setting_dict: Optional[dict]
+    file: QFile = QFile(PATH["CONFIG"] + "settings.json")
+    if file.exists():
+        res, settings_dict = read_settings(file)
+        if res and arg_key in settings_dict:
+            settings_dict[arg_key] = arg_value
+            res = write_settings(file, settings_dict)
+        else:
+            logger.error(f"Key: {arg_key} does not exist in 'settings.json'")
+    else:
+        logger.error("'settings.json' is not existed when trying to set an argument")
+    return res
+
+
+def read_settings(settings_file: QFile) -> tuple[bool, Optional[dict]]:
+    """
+    Read json from 'settings.json' and return it as dictionary.
+    :param settings_file: QFile
+    :return tuple: (bool, dictionary | None)
+    """
+    tup_res: tuple[bool, Optional[dict]] = (False, None)
+    if settings_file.open(QIODevice.ReadOnly | QIODevice.Text | QIODevice.ExistingOnly):
+        stream: QTextStream = QTextStream(settings_file)
+        settings_dict: Any = json.loads(stream.readAll())
+        if settings_dict:
+            tup_res = (True, settings_dict)
+        else:
+            logger.warning("'settings.json' is empty")
+    else:
+        logger.error("Fail to open 'settings.json' when trying to set an argument")
+    settings_file.close()
+    return tup_res
+
+
+def write_settings(settings_file: QFile, settings_dict: dict) -> bool:
+    """
+    Write json back to 'settings.json'
+    :param settings_file: QFile
+    :param settings_dict: json dictionary
+    :return: bool
+    """
+    res: bool = False
+    if settings_file.open(QIODevice.WriteOnly | QIODevice.Text | QIODevice.ExistingOnly):
+        stream: QTextStream = QTextStream(settings_file)
+        stream << json.dumps(settings_dict, indent=2)
+        res = True
+    settings_file.close()
+    return res
